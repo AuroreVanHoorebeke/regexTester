6130168ef55903a8f73e35df877ef6ba39cade0e
AuroreVanHoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-12-01 20:30:17 +0100

    cleaned comment

--- regexTesterConsole.js
+++ regexTesterConsole.js
@@ -37,37 +37,3 @@ function checker(testInput, patternInput){
 
     console.log(pattern);
 }
-
-// body.addEventListener("change", checker(testInput, checkboxI, checkboxG, checkboxM, patternInput,colorBox));
-
-// body.addEventListener("change", function (testInput, checkboxI, checkboxG, checkboxM, patternInput){
-//     let testValue = testInput.value;
-    
-//     console.log('change')
-//     let flag = "";
-
-//     if (checkboxI.checked){
-//         flag += "i";
-
-//     };
-//     if (checkboxG.checked) {
-//         flag += "g";
-
-//     };
-//     if (checkboxM.checked) {
-//         flag += "m";
-
-//     };
-
-//     let pattern = new RegExp(patternInput.value, flag);
-    
-//     if (testValue.match(pattern)) {
-//         console.log(`${testValue} has the correct pattern!`);
-//         colorBox.style.backgroundColor = "green";
-//     } else {
-//         console.log(`${testValue} has an incorrect pattern!`);
-//         colorBox.style.backgroundColor = "red";
-//     };
-
-//     console.log(pattern);
-// });
