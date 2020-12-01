08b691ae5d0d51cd39498d89e50e2e95cf640776
AuroreVanHoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-12-01 16:35:14 +0100

    push regexTester mini project

---
+++ regexTester.js
@@ -0,0 +1,123 @@
+//-----------Create divs-----------------//
+const body = document.querySelector("body");
+
+const tester = document.createElement("div");
+tester.className = "tester";
+body.appendChild(tester);
+
+
+//*-*-Checkbox I-*-*//
+const checkboxIDiv = document.createElement("div");
+checkboxIDiv.className = "checkboxIDiv";
+tester.appendChild(checkboxIDiv);
+
+const checkboxI = document.createElement("input");
+checkboxI.type = "checkbox";
+checkboxI.className = "checkboxI";
+checkboxI.id = "checkboxI";
+checkboxI.name = "insensitive";
+checkboxIDiv.appendChild(checkboxI);
+
+const labelCheckboxI = document.createElement("label");
+labelCheckboxI.for = "checkboxI";
+labelCheckboxI.innerHTML = "case insensitive";
+checkboxIDiv.appendChild(labelCheckboxI);
+
+//*-*-Checkbox G-*-*//
+const checkboxGDiv = document.createElement("div");
+checkboxGDiv.className = "checkboxGDiv";
+tester.appendChild(checkboxGDiv);
+
+const checkboxG = document.createElement("input");
+checkboxG.type = "checkbox";
+checkboxG.className = "checkboxG";
+checkboxG.id = "checkboxG";
+checkboxG.name = "global";
+checkboxGDiv.appendChild(checkboxG);
+
+const labelCheckboxG = document.createElement("label");
+labelCheckboxG.for = "checkboxG";
+labelCheckboxG.innerHTML = "global";
+checkboxGDiv.appendChild(labelCheckboxG);
+
+//*-*-Checkbox m-*-*//
+const checkboxMDiv = document.createElement("div");
+checkboxMDiv.className = "checkboxIDiv";
+tester.appendChild(checkboxMDiv);
+
+const checkboxM = document.createElement("input");
+checkboxM.type = "checkbox";
+checkboxM.className = "checkboxM";
+checkboxM.id = "checkboxM";
+checkboxM.name = "multiline";
+checkboxMDiv.appendChild(checkboxM);
+
+const labelCheckboxM = document.createElement("label");
+labelCheckboxM.for = "checkboxM";
+labelCheckboxM.innerHTML = "multiline";
+checkboxMDiv.appendChild(labelCheckboxM);
+
+//*-*-Test case-*-*//
+const testDiv = document.createElement("div");
+testDiv.className = "testDiv";
+tester.appendChild(testDiv);
+
+const testInput = document.createElement("input");
+testInput.type = "text";
+testInput.placeholder = "Case to test";
+testInput.innerHTML = "";
+testDiv.appendChild(testInput);
+
+//*-*-Pattern to verify-*-*//
+const patternDiv = document.createElement("div");
+patternDiv.className = "patternDiv";
+tester.appendChild(patternDiv);
+
+let patternInput = document.createElement("input");
+patternInput.type = "text";
+patternInput.placeholder = "Regex pattern";
+patternInput.innerHTML = "";
+patternDiv.appendChild(patternInput);
+
+//*-*-Color box-*-*//
+const colorBox = document.createElement("div");
+colorBox.className = "colorBoxDiv";
+colorBox.style.width = "100px";
+colorBox.style.height = "100px";
+colorBox.style.border = "1px solid black";
+tester.appendChild(colorBox);
+
+
+//----Event listeners----//
+
+body.addEventListener("change", function (){
+    let testValue = testInput.value;
+    
+    console.log('change')
+    let flag = "";
+
+    if (checkboxI.checked){
+        flag += "i";
+
+    };
+    if (checkboxG.checked) {
+        flag += "g";
+
+    };
+    if (checkboxM.checked) {
+        flag += "m";
+
+    };
+
+    let pattern = new RegExp(patternInput.value, flag);
+    
+    if (testValue.match(pattern)) {
+        console.log(`${testValue} has the correct pattern!`);
+        colorBox.style.backgroundColor = "green";
+    } else {
+        console.log(`${testValue} has an incorrect pattern!`);
+        colorBox.style.backgroundColor = "red";
+    };
+
+    console.log(pattern);
+});
