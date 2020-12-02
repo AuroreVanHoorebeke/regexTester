259cf618c6346883633771562cb9bf08cf053189
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-12-02 10:08:35 +0100

    installed node module and finished console version

--- regexTesterConsole.js
+++ regexTesterConsole.js
@@ -2,36 +2,21 @@ let readlineSync = require("readline-sync");
 
 let testInput = readlineSync.question("Test case: ")
 let patternInput = readlineSync.question("Pattern to test: ")
+let flags = readlineSync.question("RegEx flag(s): ")
 
 checker(testInput, patternInput);
 
 function checker(testInput, patternInput){
 
-    let testValue = testInput.value;
-    
     console.log('change')
-    let flag = "";
 
-    if (checkboxI.checked){
-        flag += "i";
+    let pattern = new RegExp(patternInput, flags);
 
-    };
-    if (checkboxG.checked) {
-        flag += "g";
-
-    };
-    if (checkboxM.checked) {
-        flag += "m";
-
-    };
-
-    let pattern = new RegExp(patternInput.value, flag);
-    
-    if (testValue.match(pattern)) {
-        console.log(`${testValue} has the correct pattern!`);
+    if (testInput.match(pattern)) {
+        console.log(`${testInput} has the correct pattern!`);
 
     } else {
-        console.log(`${testValue} has an incorrect pattern!`);
+        console.log(`${testInput} has an incorrect pattern!`);
 
     };
 
